57ff6ae635c61e3f741bd7216cad9fdc547dd770
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-01 13:44:54 +0200

    Fix import of RegistrationAPIView in user registration URL routing

--- users_app/api/urls.py
+++ users_app/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import RegistrationView
+from .views import RegistrationAPIView
 
 urlpatterns = [
-    path('registration/', RegistrationView.as_view(), name='registration'),
+    path('registration/', RegistrationAPIView.as_view(), name='registration'),
 ]
