2b9b0e2b7209d1242211ba5547a6ed7d02470b84
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-01 20:32:15 +0200

    add: include ProfileAPIView in URL patterns for user profile access

--- users_app/api/urls.py
+++ users_app/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegistrationAPIView, LoginAPIView
+from .views import RegistrationAPIView, LoginAPIView, ProfileAPIView
 
 urlpatterns = [
     path('registration/', RegistrationAPIView.as_view(), name='registration'),
     path('login/', LoginAPIView.as_view(), name='login'),
+    path('profile/<int:pk>/', ProfileAPIView.as_view(), name='profile'),
 ]
