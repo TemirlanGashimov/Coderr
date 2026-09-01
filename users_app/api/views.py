baba69a7edeb387e7448d0963d30923df32a0533
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-01 15:02:32 +0200

    add: implement LoginAPIView for user authentication

--- users_app/api/views.py
+++ users_app/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from users_app.api.serializers import RegistrationSerializer
+from users_app.api.serializers import RegistrationSerializer, LoginSerializer
 
 class RegistrationAPIView(APIView):
 
@@ -28,5 +28,28 @@ class RegistrationAPIView(APIView):
             return Response(data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginAPIView(APIView):
+
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            token, _ = Token.objects.get_or_create(user=user)
+
+            data = {
+                'token': token.key,
+                'username': user.username,
+                'email': user.email,
+                'user_id': user.id,
+            }
+
+            return Response(data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
     
 
