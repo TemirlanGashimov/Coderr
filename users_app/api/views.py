1fbe2c1da966928e9856e9fe64764b0148e0d710
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-01 13:26:25 +0200

    Add RegistrationAPIView for user registration with token generation and response data

---
+++ users_app/api/views.py
@@ -0,0 +1,32 @@
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from users_app.api.serializers import RegistrationSerializer
+
+class RegistrationAPIView(APIView):
+
+    permission_classes = [AllowAny]
+
+    def post (self, request):
+        serializer = RegistrationSerializer(data=request.data)
+
+        if serializer.is_valid():
+            saved_account = serializer.save()
+
+            token, _ = Token.objects.get_or_create(user=saved_account)
+
+            data = {
+                'token': token.key,
+                'username': saved_account.username,
+                'email': saved_account.email,
+                'user_id': saved_account.id,
+            }
+
+            return Response(data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
