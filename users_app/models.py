35ac33bf4aa2b50c3d834ed05b78f5f949cff662
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-01 19:32:29 +0200

    fix: correct business type choice and update string representation in UserProfile model

--- users_app/models.py
+++ users_app/models.py
@@ -8,17 +8,23 @@ class UserProfile(models.Model):
 
     TYPE_CHOICES = [
         ('customer', 'Customer'),
-        ('busisness-user', 'Busisness User'),
+        ('business', 'Business'),
     ]
 
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name='profile')
     type = models.CharField(
         max_length=50, choices=TYPE_CHOICES, default='customer')
+    file = models.FileField(upload_to='user_files/', default="", blank=True)
+    location = models.CharField(max_length=255, default="", blank=True)
+    tel = models.CharField(max_length=20, default="", blank=True)
+    description = models.TextField(default="", blank=True)
+    working_hours = models.CharField(max_length=100, default="", blank=True)
+
 
     class Meta:
         verbose_name = 'User Profile'
         verbose_name_plural = 'User Profiles'
 
     def __str__(self):
-        return f'{self.username} ({self.type})'
+        return f'{self.user} ({self.type})'
