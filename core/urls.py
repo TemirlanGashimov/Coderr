d7df978c85538038aaa2902f387bf2cb853a8073
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-01 12:21:14 +0200

    Add include statements for app URL configurations in core urls.py

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('users_app.urls')),
+    path('api/', include('offers_app.urls')),
+    path('api/', include('orders_app.urls')),
+    path('api/', include('reviews_app.urls')),
+    path('api/', include('dashboard_app.urls')),
 ]
