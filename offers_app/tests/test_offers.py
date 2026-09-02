c6c47bb129ee8c055cfc88f1899de083a38fe16c
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-02 21:12:25 +0200

    add: implement tests for offer creation and validation scenarios

---
+++ offers_app/tests/test_offers.py
@@ -0,0 +1,98 @@
+from django.contrib.auth.models import User
+from users_app.models import UserProfile
+from django.urls import reverse
+from django.test import TestCase
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
+from rest_framework import status
+
+from offers_app.models import Offer, OfferDetail
+
+
+class OfferBaseTestCase(TestCase):
+
+    user_type = 'business'
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='businessuser',
+            email='business@test.de',
+            password='Test12345!'
+        )
+
+        self.profile = UserProfile.objects.create(
+            user=self.user, type=self.user_type)
+        self.token, _ = Token.objects.get_or_create(user=self.user)
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.url = reverse('offers')
+        self.valid_data = {
+            'title': 'Grafikdesign-Paket',
+            'description': 'Ein Testangebot',
+            'details': [
+                {
+                    'title': 'Basic Design',
+                    'revisions': 2,
+                    'delivery_time_in_days': 5,
+                    'price': 100,
+                    'features': ['Logo Design'],
+                    'offer_type': 'basic'
+                },
+                {
+                    'title': 'Standard Design',
+                    'revisions': 5,
+                    'delivery_time_in_days': 7,
+                    'price': 200,
+                    'features': ['Logo Design', 'Visitenkarte'],
+                    'offer_type': 'standard'
+                },
+                {
+                    'title': 'Premium Design',
+                    'revisions': 10,
+                    'delivery_time_in_days': 10,
+                    'price': 500,
+                    'features': ['Logo Design', 'Visitenkarte', 'Flyer'],
+                    'offer_type': 'premium'
+                }
+            ]
+        }
+
+
+class OfferPostHappyTestCase(OfferBaseTestCase):
+
+    def test_post_create_offer(self):
+        response = self.client.post(self.url, self.valid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Offer.objects.count(), 1)
+        self.assertEqual(OfferDetail.objects.count(), 3)
+
+        offer = Offer.objects.first()
+        self.assertEqual(offer.user, self.user)
+
+
+class OfferPostUnhappyTestCase(OfferBaseTestCase):
+
+    def test_post_offer_unauthenticated(self):
+        self.client.credentials()
+        response = self.client.post(self.url, self.valid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_customer_cannot_create_offer(self):
+        self.profile.type = 'customer'
+        self.profile.save()
+        response = self.client.post(self.url, self.valid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_offer_requires_three_details(self):
+        invalid_data = self.valid_data.copy()
+        invalid_data['details'] = self.valid_data['details'][:2]
+        response = self.client.post(self.url, invalid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_offer_requires_basic_standard_premium(self):
+        invalid_data = self.valid_data.copy()
+        invalid_data['details'] = [detail.copy()
+                                   for detail in self.valid_data['details']]
+        invalid_data['details'][2]['offer_type'] = 'standard'
+        response = self.client.post(self.url, invalid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
