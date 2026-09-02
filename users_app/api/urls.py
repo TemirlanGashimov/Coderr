5159587b06b1bab26651baffe37e10ed3ed75a6c
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-02 14:36:02 +0200

    add: include CustomerProfileListAPIView in urls.py for customer profile retrieval

--- users_app/api/urls.py
+++ users_app/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegistrationAPIView, LoginAPIView, ProfileAPIView, BusinessProfileListAPIView
+from .views import RegistrationAPIView, LoginAPIView, ProfileAPIView, BusinessProfileListAPIView, CustomerProfileListAPIView
 
 urlpatterns = [
     path('registration/', RegistrationAPIView.as_view(), name='registration'),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('profile/<int:pk>/', ProfileAPIView.as_view(), name='profile'),
     path('profiles/business/', BusinessProfileListAPIView.as_view(), name='business-profiles'),
+    path('profiles/customer/', CustomerProfileListAPIView.as_view(), name='customer-profiles')
 ]
