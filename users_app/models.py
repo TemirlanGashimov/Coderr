9dcce2b9bcf1c3e2375a8b7b70fedf92c9586b48
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-02 14:34:54 +0200

    add: add uploaded_at field to UserProfile model for tracking creation time

--- users_app/models.py
+++ users_app/models.py
@@ -20,6 +20,7 @@ class UserProfile(models.Model):
     tel = models.CharField(max_length=20, default="", blank=True)
     description = models.TextField(default="", blank=True)
     working_hours = models.CharField(max_length=100, default="", blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
 
 
     class Meta:
