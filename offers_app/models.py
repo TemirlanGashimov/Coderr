cacf4ef4a6012e1255bb8829a6e54dd644d9c51d
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-02 19:50:23 +0200

    add: define Offer and OfferDetail models with relevant fields for offers management

--- offers_app/models.py
+++ offers_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Offer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='offer_images/', blank=True, null=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class OfferDetail(models.Model):
+    OFFER_TYPE_CHOICES = [
+        ('basic', 'Basic'),
+        ('standard', 'Standard'),
+        ('premium', 'Premium'),
+    ]
+
+    offer = models.ForeignKey(
+        Offer, on_delete=models.CASCADE, related_name='details')
+
+    title = models.CharField(max_length=255)
+    revisions = models.IntegerField()
+    delivery_time_in_days = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    features = models.JSONField(default=list)
+    offer_type = models.CharField(max_length=30, choices=OFFER_TYPE_CHOICES)
