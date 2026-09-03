32d97dafc88a28b0a42cbd5a04cc3cff81d218bd
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-03 21:11:15 +0200

    fix: update IsBusinessUser permission class to allow GET requests and ensure proper authentication checks

--- offers_app/api/permissions.py
+++ offers_app/api/permissions.py
@@ -2,9 +2,13 @@ from rest_framework.permissions import BasePermission
 
 
 class IsBusinessUser(BasePermission):
-	message = "Only business users can create offers."
+    message = "Only business users can create offers."
 
-	def has_permission(self, request, view):
-		if request.method != 'POST':
-			return True
-		return request.user.profile.type == 'business'
+    def has_permission(self, request, view):
+        if request.method == 'GET':
+            return True
+
+        if not request.user.is_authenticated:
+            return False
+
+        return request.user.profile.type == 'business'
