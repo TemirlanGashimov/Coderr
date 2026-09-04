d3d5ca75faf66aebfdb9e33d4add45ca30d71d26
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-04 12:48:14 +0200

    add: include OfferRetrieveUpdateDestroyAPIView in URL patterns for offer detail retrieval

--- offers_app/api/urls.py
+++ offers_app/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import OfferListCreateAPIView
+from .views import OfferListCreateAPIView, OfferRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
     path('offers/', OfferListCreateAPIView.as_view(), name='offers'),
+    path('offers/<int:pk>/', OfferRetrieveUpdateDestroyAPIView.as_view(), name='offer'),
 ]
