ea809e5d8debdcd53bad908aca3b60b0b883730a
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-09 14:30:08 +0200

    add: implement IsCreatorOffers permission class to restrict offer modifications to creators

--- offers_app/api/permissions.py
+++ offers_app/api/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
 
 class IsBusinessUser(BasePermission):
@@ -12,3 +13,12 @@ class IsBusinessUser(BasePermission):
             return False
 
         return request.user.profile.type == 'business'
+
+class IsCreatorOffers(BasePermission):
+    message = "Only Creater this Offers can canchange it."
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user == request.user
