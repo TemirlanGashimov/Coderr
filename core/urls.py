3df712236809573c1063f01853f577fd9bc5454d
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-09 16:48:05 +0200

    add: include orders_app API URLs in the main URL configuration

--- core/urls.py
+++ core/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users_app.api.urls')),
     path('api/', include('offers_app.api.urls')),
-    # path('api/', include('orders_app.api.urls')),
+    path('api/', include('orders_app.api.urls')),
     # path('api/', include('reviews_app.api.urls')),
     # path('api/', include('dashboard_app.api.urls')),
 ]
