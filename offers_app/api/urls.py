c97fa0fc98e58dde3ae414bf26003e38b52c7959
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-09 16:23:34 +0200

    add: add URL path for OfferDetailRetrieveAPIView to retrieve offer details

--- offers_app/api/urls.py
+++ offers_app/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import OfferListCreateAPIView, OfferRetrieveUpdateDestroyAPIView
+from .views import OfferListCreateAPIView, OfferRetrieveUpdateDestroyAPIView, OfferDetailRetrieveAPIView
 
 urlpatterns = [
     path('offers/', OfferListCreateAPIView.as_view(), name='offers'),
     path('offers/<int:pk>/', OfferRetrieveUpdateDestroyAPIView.as_view(), name='offer'),
+    path('offerdetails/<int:pk>/', OfferDetailRetrieveAPIView.as_view(), name='offer-detail')
 ]
