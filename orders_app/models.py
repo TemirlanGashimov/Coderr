8febc18efe66176211b9dd1c0355607e9db1d9f7
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-09 17:30:25 +0200

    add: extend Order model with offer_type and status fields including choices

--- orders_app/models.py
+++ orders_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Order(models.Model):
+
+    OFFER_TYPE_CHOICES = [
+        ('basic', 'Basic'),
+        ('standard', 'Standard'),
+        ('premium', 'Premium'),
+    ]
+
+    STATUS_CHOICES = [
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed')
+    ]
+
+    customer_user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="customer_orders")
+    business_user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="business_orders")
+    title = models.CharField(max_length=255)
+    revisions = models.IntegerField()
+    delivery_time_in_days = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    features = models.JSONField(default=list)
+    offer_type = models.CharField(max_length=30, choices=OFFER_TYPE_CHOICES)
+    status = models.CharField(
+        max_length=30, choices=STATUS_CHOICES, default='in_progress')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
