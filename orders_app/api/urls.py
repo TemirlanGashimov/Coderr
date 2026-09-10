26cd15808b73583fa0a8ff9f5b4341d9fe822b7b
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-10 21:30:30 +0200

    add: include OrderDetailAPIView in the API URLs for order detail retrieval

--- orders_app/api/urls.py
+++ orders_app/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import OrderListCreateAPIView
+from .views import OrderListCreateAPIView, OrderDetailAPIView
 
 urlpatterns = [
-    path('orders/', OrderListCreateAPIView.as_view(), name='orders')
+    path('orders/', OrderListCreateAPIView.as_view(), name='orders'),
+    path('orders/<int:pk>/', OrderDetailAPIView.as_view(), name='order-detail')
 ]
