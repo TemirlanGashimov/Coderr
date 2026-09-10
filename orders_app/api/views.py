98d777f703b692d647a2d16a2b3811463c50a838
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-10 16:57:54 +0200

    add: implement get_queryset and get_permissions methods in OrderListCreateAPIView

--- orders_app/api/views.py
+++ orders_app/api/views.py
@@ -5,11 +5,24 @@ from rest_framework import status
 from rest_framework import filters
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsCustomer
+from django.db.models import Q
 
 from .serializers import OrderSerializer
+
 
 class OrderListCreateAPIView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     permission_classes = [IsCustomer, IsAuthenticated]
     serializer_class = OrderSerializer
 
+    def get_queryset(self):
+        user = self.request.user
+        orders = Order.objects.filter(
+            Q(customer_user=user) | Q(business_user=user))
+        return orders
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [IsAuthenticated()]
+        else: 
+            return [IsCustomer(), IsAuthenticated()]
