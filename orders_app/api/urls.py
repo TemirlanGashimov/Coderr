aad420d918726b5be728864c0c10652057503c35
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-10 15:48:04 +0200

    fix: correct import statement for OrderListCreateAPIView in URLs configuration

--- orders_app/api/urls.py
+++ orders_app/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-# from .views import
+from .views import OrderListCreateAPIView
 
 urlpatterns = [
-    
+    path('orders/', OrderListCreateAPIView.as_view(), name='orders')
 ]
