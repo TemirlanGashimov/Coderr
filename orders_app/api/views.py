7102c746d423f40d297f565ca9f79ddcbe55530e
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-14 15:00:17 +0200

    add: implement OrderCountAPIView to retrieve the count of in-progress orders for a business user

--- orders_app/api/views.py
+++ orders_app/api/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from orders_app.models import Order
 from rest_framework import generics
 from rest_framework.response import Response
@@ -6,6 +7,8 @@ from rest_framework import filters
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .permissions import IsCustomer, IsBusiness
 from django.db.models import Q
+from django.shortcuts import get_object_or_404
+from rest_framework.exceptions import NotFound
 
 from .serializers import OrderSerializer, OrderStatusSerializer
 
@@ -37,3 +40,13 @@ class OrderDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
             return [IsAdminUser()]
         else:
             return [IsBusiness(), IsAuthenticated()]
+
+class OrderCountAPIView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, business_user_id):
+        business_user = get_object_or_404(User, pk=business_user_id)
+        if business_user.profile.type != 'business':
+            raise NotFound()
+        order_count = Order.objects.filter(business_user=business_user, status='in_progress').count()
+        return Response({"order_count": order_count})
