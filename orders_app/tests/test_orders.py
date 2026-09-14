39178bc9549e9ee261bb2ceef59056c7b094c73e
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-14 15:00:22 +0200

    add: implement OrderCountHappyTestCase to test order count retrieval for business users

--- orders_app/tests/test_orders.py
+++ orders_app/tests/test_orders.py
@@ -173,11 +173,12 @@ class OrderDeleteHappyTestCase(OrderBaseTestCase):
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertFalse(Order.objects.filter(pk=order.pk).exists())
 
+
 class OrderDeleteUnHappyTestCase(OrderBaseTestCase):
 
     def test_delete_order_unauthenticated(self):
         order = Order.objects.create(customer_user=self.user, business_user=self.business_user, title='Test Order',
-            revisions=3, delivery_time_in_days=5, price=10, features=['Logo Design'], offer_type='basic')
+                                     revisions=3, delivery_time_in_days=5, price=10, features=['Logo Design'], offer_type='basic')
         self.url = reverse('order-detail', kwargs={'pk': order.pk})
         self.client.credentials()
         response = self.client.delete(self.url)
@@ -185,13 +186,41 @@ class OrderDeleteUnHappyTestCase(OrderBaseTestCase):
 
     def test_delete_order_as_non_admin(self):
         order = Order.objects.create(customer_user=self.user, business_user=self.business_user,
-            title='Test Order', revisions=3, delivery_time_in_days=5, price=10, features=['Logo Design'], offer_type='basic')
+                                     title='Test Order', revisions=3, delivery_time_in_days=5, price=10, features=['Logo Design'], offer_type='basic')
         self.url = reverse('order-detail', kwargs={'pk': order.pk})
         response = self.client.delete(self.url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_delete_order_not_found(self):
         self.url = reverse('order-detail', kwargs={'pk': 99999})
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin_token.key)
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Token ' + self.admin_token.key)
         response = self.client.delete(self.url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class OrderCountHappyTestCase(OrderBaseTestCase):
+
+    def test_get_order_count_success(self):
+        order = Order.objects.create(business_user=self.business_user, status='in_progress', customer_user=self.user,
+                                     title='Test Order', revisions=3, delivery_time_in_days=5, price=10, features=['Logo Design'], offer_type='basic')
+        self.url = reverse(
+            'order-count', kwargs={'business_user_id': self.business_user.pk})
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["order_count"], 1)
+
+    def test_get_order_count_unauthenticated(self):
+        self.url = reverse(
+            'order-count', kwargs={'business_user_id': self.business_user.pk})
+        self.client.credentials()
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_order_count_business_user_not_found(self):
+        self.url = reverse('order-count', kwargs={'business_user_id': 9999999})
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Token ' + self.admin_token.key)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code,
+                         status.HTTP_404_NOT_FOUND)
