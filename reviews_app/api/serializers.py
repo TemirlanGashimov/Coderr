ea8ae3b60d53c51e4dffa6d63fee060bb0f1d27a
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-15 16:45:43 +0200

    add: implement ReviewSerializer with validation for business users and duplicate reviews

---
+++ reviews_app/api/serializers.py
@@ -0,0 +1,25 @@
+from reviews_app.models import Reviews
+from rest_framework import serializers
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+
+    rating = serializers.IntegerField(min_value=1, max_value=5)
+
+    class Meta:
+        model = Reviews
+        fields = ['id', 'business_user', 'reviewer', 'rating',
+                  'description', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'reviewer', 'created_at', 'updated_at']
+
+    def validate_business_user(self, value):
+        if value.profile.type != "business":
+            raise serializers.ValidationError(
+                "User must have a business profile.")
+        return value
+
+    def validate(self, attrs):
+
+        if Reviews.objects.filter(reviewer=self.context['request'].user, business_user=attrs['business_user']).exists():
+            raise serializers.ValidationError("You have already reviewed this business user.")
+        return attrs
