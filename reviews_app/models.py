9cd174bd8441008e45e2fbf8ea4a742be5b7fa66
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-15 15:04:29 +0200

    add: create initial migration for Reviews model with constraints

--- reviews_app/models.py
+++ reviews_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Reviews(models.Model):
+
+    business_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='business_reviews')
+    reviewer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='written_reviews')
+    rating = models.IntegerField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints=[
+            models.UniqueConstraint(
+                fields=['reviewer', 'business_user'],
+                name='unique_review_per_business_user'
+            )
+        ]
