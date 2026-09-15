73d1238db74d1d93d88ef18a677f9170983be55d
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-15 16:45:38 +0200

    add: include reviews API in URL configuration

--- core/urls.py
+++ core/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('api/', include('users_app.api.urls')),
     path('api/', include('offers_app.api.urls')),
     path('api/', include('orders_app.api.urls')),
-    # path('api/', include('reviews_app.api.urls')),
+    path('api/', include('reviews_app.api.urls')),
     # path('api/', include('dashboard_app.api.urls')),
 ]
