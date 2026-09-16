17d77c986aff3759981e26475fbabb16d3b7b847
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-16 10:41:07 +0200

    fix: update URL path for review API endpoint

--- reviews_app/api/urls.py
+++ reviews_app/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ReviewListCreateAPIView
 
 urlpatterns = [
-    path('reviews/', ReviewListCreateAPIView.as_view(), name='reviews'),
+    path('review/', ReviewListCreateAPIView.as_view(), name='review'),
 ]
