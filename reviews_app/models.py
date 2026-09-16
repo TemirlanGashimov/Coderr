42c8471aeb835782995f60f59e803380a637d056
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-16 10:40:39 +0200

    change class name

--- reviews_app/models.py
+++ reviews_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Reviews(models.Model):
+class Review(models.Model):
 
     business_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='business_reviews')
     reviewer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='written_reviews')
