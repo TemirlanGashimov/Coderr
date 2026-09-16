3c2f5a42927832bf6ae8b18528e7f30cdc4c1bd1
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-16 14:42:21 +0200

    feat: add ReviewDetailAPIView to handle individual review retrieval

--- reviews_app/api/urls.py
+++ reviews_app/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ReviewListCreateAPIView
+from .views import ReviewListCreateAPIView, ReviewDetailAPIView
 
 urlpatterns = [
     path('reviews/', ReviewListCreateAPIView.as_view(), name='reviews'),
+    path('reviews/<int:pk>/', ReviewDetailAPIView.as_view(),name='review-detail')
 ]
