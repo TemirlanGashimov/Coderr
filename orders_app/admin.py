44728c7bd718dc696092527196ff1605e0666862
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-09-17 10:41:07 +0200

    refactor: register Order model in admin with improved display and filtering options

--- orders_app/admin.py
+++ orders_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+	list_display = ('title', 'customer_user', 'business_user', 'status', 'price', 'created_at')
+	list_filter = ('status', 'offer_type', 'created_at')
+	search_fields = ('title', 'customer_user__username', 'business_user__username')
